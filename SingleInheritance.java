9cb10b218e80c737644e05c34c7e63c9adf7b196
chirramahimateja-ux <chirramahimateja@gmail.com>
2026-03-18 10:53:37 +0530

    Add files via upload

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -1,18 +1,33 @@
-class Faculty {
-    void show() {
-        System.out.println("faculty is a parent class");
-    }
+class Laptop
+{
+ void display()
+{
+ System.out.println("This is Laptop(parent class)");
+} 
 }
-
-class Student extends Faculty {
-    void display() {
-        System.out.println("student is a child class");
-    }
+  class Hp extends Laptop
+{
+ void display()
+{
+ System.out.println("Hp is a child class");
 }
-
-public class SingleInheritance {
-    public static void main(String[] args) {
-        Student s = new Student();
-        s.show();    
-        s.display();     }
+}
+class Dell extends Hp
+{
+ void display()
+{
+ //super.display();
+ System.out.println("Dell is a child 2");
+}
+}
+ 
+ class SingleInheritance
+{
+ public static void main(String[] args)
+{
+  Laptop L = new Hp();
+ Laptop a=new Dell();
+ L.display();
+a.display();
+}
 }
